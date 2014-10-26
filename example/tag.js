71f91f347e96348b13f0afe0000b8c8ee3613797
James Halliday <mail@substack.net>
2014-10-26 12:40:34 +0000

    tag example

--- example/tag.js
+++ example/tag.js
@@ -2,5 +2,5 @@ var wikidb = require('../');
 var db = require('level')('/tmp/wiki.db');
 
 var wdb = wikidb(db, { dir: '/tmp/wiki.blob' });
-var s = wdb.search(process.argv.slice(2));
+var s = wdb.byTag(process.argv[2]);
 s.on('data', console.log);
