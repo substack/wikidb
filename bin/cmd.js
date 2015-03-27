4a48b6ed8f4c8d110343c63b224be24bc56a7e7b
James Halliday <mail@substack.net>
2015-03-28 12:51:14 +1300

    history, future, drop party

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
-var level = require('level-party');
+var level = require('level');
 var minimist = require('minimist');
 var wikidb = require('../');
 var path = require('path');
 var defined = require('defined');
 var mkdirp = require('mkdirp');
+
+var showHistory = require('forkdb/bin/lib/show_history.js');
+var showFuture = require('forkdb/bin/lib/show_future.js');
 
 var argv = minimist(process.argv.slice(2), {
     alias: { d: 'datadir' },
@@ -41,6 +44,12 @@ else if (cmd === 'recent') {
     r.on('data', console.log);
     r.on('end', function () { db.close() });
 }
+else if (cmd === 'history') {
+    showHistory(wdb, argv._[1], function () { db.close() });
+}
+else if (cmd === 'future') {
+    showFuture(wdb, argv._[1], function () { db.close() });
+}
 else if (cmd === 'by-tag') {
     var r = wdb.byTag(argv._[1]);
     r.on('data', console.log);
@@ -52,9 +61,11 @@ else if (cmd === 'search') {
     r.on('end', function () { db.close() });
 }
 else if (cmd === 'get') {
-    var r = wdb.get(argv._[1]);
-    r.pipe(process.stdout);
-    r.on('end', function () { db.close() });
+    wdb.get(argv._[1], function (err, meta) {
+        if (err) error(err)
+        else console.log(meta)
+        db.close();
+    });
 }
 else if (cmd === 'sync' || cmd === 'pull' || cmd === 'push') {
     var r = wdb.replicate({ mode: cmd });
